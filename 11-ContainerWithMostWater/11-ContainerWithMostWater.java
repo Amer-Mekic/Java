d5adef3133d9d9beb4753b02fd6060ee0b6f5643
Amer-Mekic <159288777+Amer-Mekic@users.noreply.github.com>
2025-02-12 19:28:36 +0100

    File updated at 12. 02. 2025. 19:28:37

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -7,9 +7,9 @@ class Solution {
             int min = Math.min(heights[i], heights[j]);
             int water = (j-i)*min;
             maxWater = Math.max(water, maxWater);
-            if(min == heights[i])
+            while(min >= heights[i] && i<j)
                 i++;
-            else if(min == heights[j])
+            while(min >= heights[j] && i<j)
                 j--;
         }
         return maxWater;
