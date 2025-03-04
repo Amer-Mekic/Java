d5df64f8fd040d42527e087122fbaa30cb9322b7
Amer-Mekic <159288777+Amer-Mekic@users.noreply.github.com>
2025-03-04 21:40:50 +0100

    File updated at 04. 03. 2025. 21:40:49

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,19 +1,22 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        int max = -1;
+    static{
+        for(int i =0 ; i<500 ; i++){
+            majorityElement(new int[]{});
+        }
+    }
+    public static int majorityElement(int[] nums) {
+        int ele = 0;
         int count = 0;
-        for(int i = 0; i<nums.length; i++){
+        for(int num : nums){
             if(count==0){
-                max = nums[i];
+                ele = num;
                 count++;
-            }
-            else{
-                if(nums[i]==max)
-                    count++;
-                else
-                    count--;
+            }else if(ele==num){
+                count++;
+            }else{
+                count--;
             }
         }
-        return max;
+        return ele;
     }
 }
