03b9e4539cc99fdf9c14d002c51f59b3e14b1f3f
Amer-Mekic <159288777+Amer-Mekic@users.noreply.github.com>
2025-04-08 17:29:11 +0200

    File updated at 08. 04. 2025. 17:29:11

--- 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.java
+++ 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.java
@@ -1,24 +1,23 @@
-// Last updated: 08. 04. 2025. 17:28:33
+// Last updated: 08. 04. 2025. 17:29:11
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        List<Boolean> list = new ArrayList<>(candies.length);
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for(int i = 0;i<candies.length;i++){
-            if(candies[i]>max)
-                max = candies[i];
-            if(candies[i]<min)
-                min = candies[i];
-        }
-        int maxOld = max;
-        for(int i = 0;i<candies.length;i++){
-            if((candies[i]+extraCandies)>=maxOld){
-                max = candies[i]+extraCandies;
-                list.add(true);
+        int n=candies.length;
+        ArrayList<Boolean> arr=new ArrayList<>();
+        int max=0;
+        for(int i=0;i<n;i++){
+            if(candies[i]>max){
+                max=candies[i];
             }
-            else
-                list.add(false);
         }
-        return list;
+        for(int i=0;i<n;i++){
+            if(candies[i]+extraCandies>=max){
+                arr.add(true);
+            }else{
+                arr.add(false);
+            }
+
+        }
+
+        return arr;
     }
 }
