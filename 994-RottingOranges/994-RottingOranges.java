63354f78153b2719d95b20623b690e22707f4b49
Amer-Mekic <159288777+Amer-Mekic@users.noreply.github.com>
2025-05-03 21:22:38 +0200

    File updated at 03. 05. 2025. 21:22:38

--- 994-RottingOranges/994-RottingOranges.java
+++ 994-RottingOranges/994-RottingOranges.java
@@ -1,18 +1,18 @@
-// Last updated: 03. 05. 2025. 20:59:19
-/* The isBadVersion API is defined in the parent class VersionControl.
-      boolean isBadVersion(int version); */
-
-public class Solution extends VersionControl {
-    public int firstBadVersion(int n) {
-        int l = 0;
-        int r = n;
-        while(l<=r){
-            int mid = l+(r-l)/2;
-            if(isBadVersion(mid))
-                r = mid-1;
+// Last updated: 03. 05. 2025. 21:22:38
+class Solution {
+    public int longestPalindrome(String s) {
+        HashMap<Character, Integer> m = new HashMap<>();
+        int odd = 0;
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            m.put(c, m.getOrDefault(c, 0)+1);
+            if((m.get(c) & 1)==1)
+                odd++;
             else
-                l = mid+1;
+                odd--;
         }
-        return l;
+        if(odd>1)
+            return s.length()-odd+1;
+        return s.length();
     }
 }
