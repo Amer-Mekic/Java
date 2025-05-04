54c4cd4fdb81e67b1805fb630845de3cd5c548c9
Amer-Mekic <159288777+Amer-Mekic@users.noreply.github.com>
2025-05-04 17:03:28 +0200

    File updated at 04. 05. 2025. 17:03:29

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
@@ -1,31 +1,25 @@
-// Last updated: 04. 05. 2025. 17:02:16
+// Last updated: 04. 05. 2025. 17:03:29
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int l = 0;
-        int r = numbers.length-1;
-        while(l<r){
-            System.out.println("sum l + r: "+(numbers[r]+numbers[l]));
-            System.out.println("Left: "+numbers[l]);
-            System.out.println("Right "+numbers[r]);
-            if(numbers[r]+numbers[l] == target){
-                return new int[]{l+1,r+1};
-            }
-            /*
-            if(numbers[mid]-Math.abs(numbers[l]) == target){
-                return new int[]{l+1,mid+1};
-            }
-            if(numbers[r]-Math.abs(numbers[mid]) == target){
-                return new int[]{l+1,mid+1};
-            }
-            */
-            else if(numbers[r]+numbers[l]<target){
-                l++;
-            }
-            else{
-                r--;
-            }
+        int p1 = 0, p2 = numbers.length - 1;
+        while (p1 < p2) {
+            int r = numbers[p1] + numbers[p2];
+            if (r == target) {
+                return new int[] {p1+1,p2+1};
+            } else if (r > target) p2 = binarySearch(numbers,p1+1,p2-1,target-numbers[p1]);
+            else p1 = binarySearch(numbers,p1+1,p2-1,target-numbers[p2]);
         }
-        // -4
-        return new int[]{-1, -1};
+        return null;
+    } 
+
+    public static int binarySearch(int[] arr, int low, int high, int t) {
+        int mid = 0;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if (arr[mid] == t) return mid;
+            else if (arr[mid] > t) high = mid - 1;
+            else low = mid + 1;
+        }
+        return mid;
     }
 }
