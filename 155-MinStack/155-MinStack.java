7e3c15c67265fc41f26c9818fc271cbcfa744be6
Amer-Mekic <159288777+Amer-Mekic@users.noreply.github.com>
2025-07-02 16:47:28 +0200

    File updated at 02. 07. 2025. 16:47:29

--- 155-MinStack/155-MinStack.java
+++ 155-MinStack/155-MinStack.java
@@ -1,40 +1,20 @@
-// Last updated: 01. 07. 2025. 21:22:11
-class MinStack {
-
-    private Stack<Integer> s;
-    private Stack<Integer> minS;
-    
-    public MinStack() {
-        s = new Stack<>();
-        minS = new Stack<>();
-    }
-    
-    public void push(int val) {
-        s.push(val);
-        if(minS.isEmpty() || s.peek()<=minS.peek())
-            minS.push(val);
-    }
-    
-    public void pop() {
-        int temp =  s.pop();
-        if(minS.peek()==temp)
-            minS.pop();
-    }
-    
-    public int top() {
-        return s.peek();
-    }
-    
-    public int getMin() {
-        return minS.peek();
+// Last updated: 02. 07. 2025. 16:47:29
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1)
+            return x;
+        int l = 0;
+        int r = x/2;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            System.out.println("Current mid: "+mid+" left: "+l+" right: "+r);
+            if(mid*mid == x)
+                return mid;
+            if((long)mid*mid>(long)x)
+                r=mid-1;
+            else
+                l=mid+1;
+        }
+        return r;
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
