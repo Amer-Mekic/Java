5eb1e9fb90b91300731e8cad7d102fba42350430
Amer-Mekic <159288777+Amer-Mekic@users.noreply.github.com>
2025-07-02 16:50:07 +0200

    File updated at 02. 07. 2025. 16:50:08

--- 155-MinStack/155-MinStack.java
+++ 155-MinStack/155-MinStack.java
@@ -1,20 +1,26 @@
-// Last updated: 02. 07. 2025. 16:47:29
+// Last updated: 02. 07. 2025. 16:50:08
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0 || x == 1)
+        if (x == 0 || x == 1) {
             return x;
-        int l = 0;
-        int r = x/2;
-        while(l<=r){
-            int mid = l+(r-l)/2;
-            System.out.println("Current mid: "+mid+" left: "+l+" right: "+r);
-            if(mid*mid == x)
-                return mid;
-            if((long)mid*mid>(long)x)
-                r=mid-1;
-            else
-                l=mid+1;
         }
-        return r;
+        
+        int left = 2;
+        int right = x / 2;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long result = (long)mid * mid;
+            
+            if (result == x) {
+                return mid;
+            } else if (result < x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return right;
     }
 }
