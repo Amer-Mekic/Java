231671d09ca6274bea65852d0cf585d93a3539d7
Amer-Mekic <159288777+Amer-Mekic@users.noreply.github.com>
2025-07-03 00:20:20 +0200

    Create 110-BalancedBinaryTree

---
+++ 110-BalancedBinaryTree/110-BalancedBinaryTree.java
@@ -0,0 +1,38 @@
+// Last updated: 03. 07. 2025. 00:20:20
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null)
+            return true;
+        if(dfs(root)==-1)
+            return false;
+        return true;
+    }
+    public int dfs(TreeNode root){
+        if(root==null)
+            return 0;
+        int l = dfs(root.left);
+        int r = dfs(root.right);
+        System.out.println("Node: " + root.val+ " l height: "+l);
+        System.out.println("Node: " + root.val+ " r height: "+ r);
+        if(l==-1 || r==-1)
+            return -1;
+        if(Math.abs(l-r)>1)
+            return -1;
+        return Math.max(l, r)+1;
+    }
+}
